4f1098762e0c96840c79c9f93865f52dee6c5951
shira <shira79@users.noreply.github.com>
2024-10-27 23:29:16 +0900

    p182 number以外も受け入れる

--- src/p182.ts
+++ src/p182.ts
@@ -1,4 +1,4 @@
-function map(array: number[], callback: (value: number) => number ): number[]{
+function map<Argument,Return>(array: Argument[], callback: (value: Argument) => Return): Return[]{
     const result = [];
     for (const value of array){
         result.push(callback(value));
@@ -10,5 +10,24 @@ function twice(value: number):number{
     return value * 2;
 }
 
-const numbers = [1,2,3,4,5];
-console.log(map(numbers, twice))
+const arr1 = [1,2,3,4,5];
+console.log(map<number, number>(arr1, twice))
+
+function addSuffix(suffix: string){
+    return (str: string) => {
+        return str + suffix;
+    }
+}
+const arr2 = ['apple', 'banana', 'cherry'];
+console.log(map<string, string>(arr2, addSuffix('!!!')))
+
+function repeat (value: number, char:string): string{
+    let result = '';
+    for (let i = 0; i < value; i++){
+        result += char;
+    }
+    return result;
+}
+
+const arr3 = [10,20, 30];
+console.log(map<number, string>(arr3, (value) => repeat(value, 'z')))
