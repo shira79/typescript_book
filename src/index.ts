68e48ee97c913ad163334b0e7992f8ed6bf9fb6d
shira <shira79@users.noreply.github.com>
2024-11-20 01:16:49 +0900

    p310

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-const message:string  =  'Hello World' ;
-console.log(message);
+
+
+type Option<T> = {
+    value: T;
+    label: 'some';
+} | {
+    label: 'none';
+}
+
+const someOption: Option<number> = {
+    value: 42,
+    label: 'some'
+}
+
+const noneOption: Option<number> = {
+    label: 'none'
+}
+
+function test(option: Option<number>) {
+    if (option.label === 'some') {
+        console.log(option.value);
+    }
+}
+
+test(someOption)
+test(noneOption);
+
+
